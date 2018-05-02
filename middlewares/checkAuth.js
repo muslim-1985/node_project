4da0ee9a9e86e69bcc1a5ed366b41d0917e38eb6
Muslim <muslim.krum.85@gmail.com>
2018-05-02 22:10:10 +0300

    auth dev  complete

--- middlewares/checkAuth.js
+++ middlewares/checkAuth.js
@@ -4,8 +4,8 @@ module.exports = {
     checkAuth (req, res, next) {
         passport.authenticate('jwt', { session: false }, (err, decryptToken, jwtError) => {
             if(jwtError) {
-                console.log(jwtError);
-                return res.status(500).send('Ошибка аутентификации, передан неверный токен');
+                //console.log(jwtError);
+                return res.status(500).json({err:'Ошибка аутентификации, передан неверный токен', jwtError});
             }
             req.user = decryptToken;
             next()
